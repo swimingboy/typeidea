f1f34519d75986c716c771017f3fea72ed8f7160
swimingboy <swimingboy@sohu.com>
2021-03-06 17:03:46 +0800

    '初始化提交'

---
+++ typeidea/settings/develop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'linghang'
+
+from .base import *  # NOQA
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
