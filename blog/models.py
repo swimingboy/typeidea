bc9831dce911d2c0c9e355e1b33545db0dea3517
swimingboy <swimingboy@sohu.com>
2021-03-06 18:59:40 +0800

    测试1

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,10 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL,
                                          choices=STATUS_ITEMS, verbose_name='状态')
+    is_nav = models.BooleanField(default=False, verbose_name='是否为导航')
+    owner = models.ForeignKey(User, verbose_name='作者')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = verbose_name_plural = '分类'
+
