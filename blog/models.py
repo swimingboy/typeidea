7d51ed662dcfb1caf8fa218903422726b5ff53ac
swimingboy <swimingboy@sohu.com>
2021-03-06 17:46:09 +0800

    '初始化提交'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除')
+    )
