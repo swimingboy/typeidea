00c126a1e209fc6bd17fd96fd54a93ea8657aad8
swimingboy <swimingboy@sohu.com>
2021-03-06 18:20:26 +0800

    测试

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-
-class Category(models.Model):
-    STATUS_NORMAL = 1
-    STATUS_DELETE = 0
-    STATUS_ITEMS = (
-        (STATUS_NORMAL, '正常'),
-        (STATUS_DELETE, '删除')
-    )
-
-    name = models.CharField(max_length=50, verbose_name='民G')
+from django.db import modelsfrom django.contrib.auth.models import User# Create your models here.class Category(models.Model):    STATUS_NORMAL = 1    STATUS_DELETE = 0    STATUS_ITEMS = (        (STATUS_NORMAL, '正常'),        (STATUS_DELETE, '删除')    )
